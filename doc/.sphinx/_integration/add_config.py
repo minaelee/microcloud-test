3e72452b5032f82f579297286b4d572c6266ea34
Minae Lee <minae.lee@canonical.com>
2026-01-14 12:28:22 -0700

    update configs
    Signed-off-by: Minae Lee <minae.lee@canonical.com>

--- doc/.sphinx/_integration/add_config.py
+++ doc/.sphinx/_integration/add_config.py
@@ -13,11 +13,22 @@ html_context['microovn_tag'] = "../microovn/_static/microovn.png"
 custom_html_js_files.append('rtd-search.js')
 
 if project == "LXD":
-    html_baseurl = "https://documentation.ubuntu.com/lxd/en/latest/"
+    html_baseurl = "https://documentation.ubuntu.com/lxd/latest/"
+    html_js_files.append('rtd-search.js')
+    tags.add('integrated')
 elif project == "MicroCeph":
     html_baseurl = "https://canonical-microceph.readthedocs-hosted.com/en/latest/"
+    # Override default header templates
+    templates_path = globals().get('templates_path', []) + [".sphinx/_templates"]
+    # Override default header styles
+    html_static_path = globals().get('html_static_path', []) + [".sphinx/_static"]
+    html_css_files = globals().get('html_css_files', []) + ['override-header.css']
+    # Add rtd-search.js to the list of JS files
+    html_js_files = globals().get('html_js_files', []) + ['rtd-search.js']
+    # Add "integrated" to the list of custom tags
+    custom_tags = globals().get('custom_tags', []) + ['integrated']
 elif project == "MicroOVN":
     html_baseurl = "https://canonical-microovn.readthedocs-hosted.com/en/latest/"
+    custom_html_js_files.append('rtd-search.js')
+    custom_tags.append('integrated')
 
-# Include integrated tag
-custom_tags.append('integrated')
